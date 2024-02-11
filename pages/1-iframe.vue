870cc73b03c95aa1347ef1972feb00a0b8b29bb4
Hermann EHO <hermannleboss@gmail.com>
2024-02-11 09:38:45 +0000

    feat: Add iframe elements to 1-iframe.vue

---
+++ pages/1-iframe.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Try simple iframe</h1>
+    <iframe
+    id="inlineFrameExample"
+    title="Inline Frame Example"
+    width="300"
+    height="200"
+    src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+  </iframe>
+  <iframe
+    id="inlineFrameExample"
+    title="Inline Frame Example"
+    width="300"
+    height="200"
+    src="https://www.tiktok.com/">
+  </iframe>
+  <iframe
+    id="inlineFrameExample"
+    title="Inline Frame Example"
+    width="300"
+    height="200"
+    src="https://www.tiktok.com/@sami.et09/video/7333131178255469830">
+  </iframe>
+  </div>
+</template>
+
+
+<style scoped></style>
