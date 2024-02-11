1e2d93b6be568398e548ec94e1e1214c0eb24d7d
Hermann EHO <hermannleboss@gmail.com>
2024-02-11 10:45:23 +0000

    feat: fetch embedded html from api

--- pages/2-api.vue
+++ pages/2-api.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <h1>Try the API fron tiktok doc</h1>
-        <h2>Creatore page</h2>
+        <h1>Try the API from tiktok doc</h1>
+        <h2>Creator page</h2>
         <ClientOnly fallback-tag="span" fallback="Loading tiktok creator page">
             <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@sami.et09" data-unique-id="sami.et09"
                 data-embed-type="creator" style="max-width: 780px; min-width: 288px">
@@ -10,10 +10,33 @@
                 </section>
             </blockquote>
         </ClientOnly>
+
+        <h2>Fetch from api</h2>
+        <div>
+            <div v-if="pending">Loading...</div>
+            <div v-if="error">Error : {{ error.message }}</div>
+            <div v-if="data" ove>
+                <h3>Embedded script</h3>
+                <div v-html="removeScriptTag(data.html)"></div>
+                <h3>Fetch Data</h3>
+                <div style="overflow: auto;">
+                    
+                <pre>{{ data }}</pre>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+
+const { data, pending, error, refresh } = useFetch(
+    "https://www.tiktok.com/oembed?url=https://www.tiktok.com/@dineiro.off/video/7333638669569428768"
+);
+
+const removeScriptTag = (html: string) => {
+    return html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
+}
 </script>
 
 <style scoped></style>
