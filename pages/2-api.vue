4302dfc3e52ca1d87da73386e192c5f8fafbdc86
Hermann EHO <hermannleboss@gmail.com>
2024-02-11 10:13:03 +0000

    feat: Add API page with TikTok creator embed

---
+++ pages/2-api.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Try the API fron tiktok doc</h1>
+        <h2>Creatore page</h2>
+        <ClientOnly fallback-tag="span" fallback="Loading tiktok creator page">
+            <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@sami.et09" data-unique-id="sami.et09"
+                data-embed-type="creator" style="max-width: 780px; min-width: 288px">
+                <section>
+                    <a target="_blank" href="https://www.tiktok.com/@sami.et09?refer=creator_embed">@sami.et09</a>
+                </section>
+            </blockquote>
+        </ClientOnly>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
