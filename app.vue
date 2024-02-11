2b5d18ce3c4002f2e0def1b2aabd9b5df64d867e
Hermann EHO <hermannleboss@gmail.com>
2024-02-11 10:45:57 +0000

    feat: add back to home link and embed TikTok script

--- app.vue
+++ app.vue
@@ -1,8 +1,21 @@
 <template>
-  <NuxtPage>  
-    
-  </NuxtPage>
+  <div>
+    <!-- Back to home link -->
+    <nuxt-link to="/" style="font-size: 25px;">Back to home</nuxt-link>
+    <NuxtPage>  
+      
+    </NuxtPage>
+  </div>
 </template>
+<script lang="ts" setup>
+useHead({
+  script: [
+    {
+      async: true,
+      src: 'https://www.tiktok.com/embed.js'
+    }
+  ]
+})</script>
 
 <style>
 * {
