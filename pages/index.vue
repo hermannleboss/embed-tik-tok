2e2fd40142b5f10aaf9e2f423682b33630860c80
Hermann EHO <hermannleboss@gmail.com>
2024-02-11 09:34:36 +0000

    feat: add index.vue file with initial page structure

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nuxt-link v-for="page in pages" :to="page.path" style="display: block"
+      ><h2>{{ page.name }}</h2></nuxt-link
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+const pages = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+];
+</script>
+
+<style scoped></style>
